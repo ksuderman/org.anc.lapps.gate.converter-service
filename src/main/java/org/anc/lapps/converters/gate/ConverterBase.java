46bdf316c385ebf4439e74e8260fee0aa1cbec22
Keith Suderman <suderman@anc.org>
2014-05-06 12:45:19 -0400

    Refactored package.

---
+++ src/main/java/org/anc/lapps/converters/gate/ConverterBase.java
@@ -0,0 +1,99 @@
+package org.anc.lapps.converters.gate;
+
+import gate.Gate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.anc.lapps.logging.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+/**
+ * @author Keith Suderman
+ */
+public abstract class ConverterBase
+{
+   private static final Logger logger = LoggerFactory.getLogger(ConverterBase.class);
+   private static Boolean initialized = false;
+
+   protected Throwable savedException = null;
+   protected static final Configuration K = new Configuration();
+
+   public ConverterBase()
+   {
+      synchronized (initialized) {
+         if (!initialized)
+         {
+            logger.info("Initializing the GATE subsystem.");
+            initialized = true;  // We only try this once.
+            try
+            {
+               File gateHome = new File(K.GATE_HOME);
+               if (!gateHome.exists())
+               {
+                  logger.error("Gate home not found: " + gateHome.getPath());
+                  savedException = new FileNotFoundException(K.GATE_HOME);
+                  return;
+               }
+               logger.info("Gate home: " + K.GATE_HOME);
+               File plugins = new File(K.PLUGINS_HOME);
+               if (!plugins.exists())
+               {
+                  logger.error("Gate plugins not found: " + plugins.getPath());
+                  savedException = new FileNotFoundException(K.PLUGINS_HOME);
+                  return;
+               }
+               logger.info("Plugins home: " + K.PLUGINS_HOME);
+               File siteConfig = new File(K.SITE_CONFIG);
+               if (!siteConfig.exists())
+               {
+                  logger.error("Site config not found: " + siteConfig.getPath());
+                  savedException = new FileNotFoundException(K.SITE_CONFIG);
+                  return;
+               }
+               logger.info("Site config: " + K.SITE_CONFIG);
+               File userConfig = new File(K.USER_CONFIG);
+               if (!userConfig.exists())
+               {
+                  logger.error("User config not found: " + userConfig.getPath());
+                  savedException = new FileNotFoundException(K.USER_CONFIG);
+                  return;
+               }
+               logger.info("User config: " + K.USER_CONFIG);
+               Gate.setGateHome(gateHome);
+               Gate.setSiteConfigFile(siteConfig);
+               Gate.setPluginsHome(plugins);
+               Gate.setUserConfigFile(userConfig);
+
+               try
+               {
+                  logger.info("Initializing GATE");
+                  Gate.init();
+               }
+               catch (Exception e)
+               {
+                  logger.error("Error initializing GATE.", e);
+                  savedException = e;
+                  return;
+               }
+
+               File[] files = plugins.listFiles();
+               for (File directory : files)
+               {
+                  if (directory.isDirectory())
+                  {
+                     logger.info("Registering plugin: " + directory.getPath());
+                     Gate.getCreoleRegister().registerDirectories(directory.toURI().toURL());
+                  }
+               }
+            }
+            catch (Exception e)
+            {
+               logger.error("Unable to configure GATE.", e);
+               logger.warn(e.getMessage());
+            }
+
+         }
+      }
+}
+}
---
+++ src/main/java/org/anc/lapps/converters/gate/JsonToGateConverter.java
@@ -0,0 +1,68 @@
+package org.anc.lapps.converters.gate;
+
+import gate.Document;
+import gate.Factory;
+import org.anc.lapps.gate.serialization.GateSerializer;
+import org.anc.lapps.serialization.Container;
+import org.lappsgrid.api.Data;
+import org.lappsgrid.api.WebService;
+import org.lappsgrid.core.DataFactory;
+import org.lappsgrid.discriminator.DiscriminatorRegistry;
+import org.lappsgrid.discriminator.Types;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Keith Suderman
+ */
+public class JsonToGateConverter extends ConverterBase implements WebService
+{
+   protected static Logger logger = LoggerFactory.getLogger(JsonToGateConverter.class);
+
+   public JsonToGateConverter()
+   {
+
+   }
+
+   public long[] produces()
+   {
+      return new long[] { Types.GATE };
+   }
+
+   public long[] requires()
+   {
+      return new long[] { Types.JSON };
+   }
+
+   public Data configure(Data input)
+   {
+      return DataFactory.error("Unsupported operation.");
+   }
+
+   public Data execute(Data input)
+   {
+      //System.err.println("Invoking the JsonToGateConverter service.");
+      if (input.getDiscriminator() != Types.JSON) {
+         logger.error("Invalid input discriminator. Expected JSON but found " + DiscriminatorRegistry.get(input.getDiscriminator()));
+         return DataFactory.error("Invalid input type. Expected JSON (" + Types.JSON + ")");
+      }
+      Data result = null;
+      try
+      {
+         logger.debug("Converting JSON to GATE.");
+         Container container = new Container(input.getPayload());
+         logger.trace("Container created.");
+         Document document = GateSerializer.convertToDocument(container);
+         logger.trace("Document created.");
+         result = new Data(Types.GATE, document.toXml());
+         Factory.deleteResource(document);
+      }
+      catch (Exception e)
+      {
+         String message = "Unable to convert to GATE document";
+         logger.error(message, e);
+         result = DataFactory.error(message, e);
+      }
+      return result;
+   }
+}
